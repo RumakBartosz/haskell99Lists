98bc59c43371afef30e7f1ac3e4407ea90937ab0
mion <27711689+RumakBartosz@users.noreply.github.com>
2019-07-30 15:27:44 +0200

    function 4 - myLength and function 5 - myReverse completed

--- out/test/haskell99Lists/Spec.hs
+++ out/test/haskell99Lists/Spec.hs
@@ -3,10 +3,29 @@ import Lib
 
 main :: IO ()
 main = hspec $
-  describe "elementAt tests" $ do
-    it "returns correct example value" $
-      elementAt [1,2,3,4,5] 3 `shouldBe` (3 :: Int)
-    it "returns correct value on single element" $
-      elementAt [1] 1 `shouldBe` (1 :: Int)
-    it "works correctly with characters" $
-      elementAt ['a', 'z', 'x', 'c'] 2 `shouldBe` ('z' :: Char)
+  describe "all functions tests" $ do
+    describe "elementAt tests" $ do
+      it "returns correct example value" $
+        elementAt [1,2,3,4,5] 3 `shouldBe` (3 :: Int)
+      it "returns correct value on single element" $
+        elementAt [1] 1 `shouldBe` (1 :: Int)
+      it "works correctly with characters" $
+        elementAt ['a', 'z', 'x', 'c'] 2 `shouldBe` ('z' :: Char)
+
+    describe "myLength tests" $ do
+      it "returns correct example value" $
+        myLength [123, 234, 345] `shouldBe` (3 :: Int)
+      it "returns correct value on single element" $
+        myLength [1] `shouldBe` (1 :: Int)
+      it "works correctly with characters" $
+        myLength ['a','b','z','k'] `shouldBe` (4 :: Int)
+
+
+    describe "myReverse tests" $ do
+      it "returns correct example value" $
+        myReverse [123, 234, 345] `shouldMatchList` [345, 234, 123]
+      it "returns correct value on single element" $
+        myReverse [1] `shouldMatchList` [1]
+      it "works correctly with characters" $
+        myReverse ['a','b','z','k'] `shouldMatchList` ['k','z','b','a']
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( elementAt
+    ( elementAt, myLength, myReverse
     ) where
 
 elementAt :: [a] -> Int -> a
@@ -8,3 +8,13 @@ elementAt [x] 1 = x
 elementAt (x:_) 1 = x
 elementAt [x] y = error "Not enough elements"
 elementAt (x:xs) y = elementAt xs (y - 1)
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength [x] = 1
+myLength (x:xs) = 1 + myLength xs
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse [x] = [x]
+myReverse (x:xs) = myReverse xs ++ [x]
